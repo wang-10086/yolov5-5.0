570fe78d8b676e8d491ca77616189ad78bbd3692
17262 <1726284569@qq.com>
2022-08-25 09:49:32 +0800

    update

--- mainwindow.py
+++ mainwindow.py
@@ -13,19 +13,13 @@ from tkinter import filedialog
 
 from model_load import model_load
 from UiMainwindow import Ui_MainWindow
-from image_detect_thread import ImageDetectThread
 from utils.datasets import LoadStreams, LoadImages
 from utils.general import check_img_size, check_requirements, check_imshow, non_max_suppression, apply_classifier, \
     scale_coords, xyxy2xywh, strip_optimizer, set_logging, increment_path
 from utils.plots import plot_one_box
 from utils.torch_utils import select_device, load_classifier, time_synchronized
 
-global model
-global quit_flag
-global conf_thres
-global iou_thres
-global detect_frequency
-global fps
+global model, device_id,  quit_flag, conf_thres, iou_thres, detect_frequency, fps
 
 
 class MyMainWindow(QMainWindow, Ui_MainWindow):
@@ -35,16 +29,11 @@ class MyMainWindow(QMainWindow, Ui_MainWindow):
     signal_4 = pyqtSignal(object)  # 发送信号,用于向子线程发送iou_thres
 
     def __init__(self, parent=None):
-        global model
-        global quit_flag
-        global conf_thres
-        global iou_thres
-        global detect_frequency
-        global fps
+        global model, device_id,  quit_flag, conf_thres, iou_thres, detect_frequency, fps
 
         quit_flag = 0
         weights = 'signal.pt'
-        device = '0'
+        device_id = '0'
 
         # 界面初始化
         super(MyMainWindow, self).__init__(parent)
@@ -59,6 +48,7 @@ class MyMainWindow(QMainWindow, Ui_MainWindow):
         self.spinBox_2.valueChanged.connect(self.refresh_fps)
         self.image_thread = None  # 初始化图片检测线程
         self.video_thread = None  # 初始化视频检测线程
+        self.realtime_thread = None     # 初始化实时检测线程
 
         conf_thres = self.horizontalSlider.value()
         iou_thres = self.horizontalSlider_2.value()
@@ -66,7 +56,7 @@ class MyMainWindow(QMainWindow, Ui_MainWindow):
         fps = self.spinBox_2.value()
 
         # 加载模型
-        model = model_load(weights, device=device)
+        model = model_load(weights, device=device_id)
         print('模型加载完成')
 
     def display(self, img):
@@ -116,8 +106,7 @@ class MyMainWindow(QMainWindow, Ui_MainWindow):
         if self.radioButton.isChecked() == 1:
             self.label_10.setVisible(0)
             self.label_11.setVisible(0)
-            self.image_thread = ImageDetectThread(model=model, conf_thres=self.horizontalSlider.value(),
-                                                  iou_thres=self.horizontalSlider_2.value())
+            self.image_thread = ImageDetectThread()
             self.image_thread.signal.connect(self.display)
             self.image_thread.signal2.connect(self.print_ifo)
             self.image_thread.start()
@@ -128,7 +117,7 @@ class MyMainWindow(QMainWindow, Ui_MainWindow):
             quit_flag = 0
             self.label_10.setVisible(0)
             self.label_11.setVisible(0)
-            self.video_thread = VideoDetectThread(model=model)
+            self.video_thread = VideoDetectThread()
             self.video_thread.signal.connect(self.display)
             self.video_thread.signal2.connect(self.print_ifo)
             self.video_thread.signal3.connect(self.progress)
@@ -136,9 +125,120 @@ class MyMainWindow(QMainWindow, Ui_MainWindow):
 
         # 实时检测
         elif self.radioButton_3.isChecked() == 1:
-            self.label_10.setVisible(1)
-            self.label_11.setVisible(1)
-            self.label_11.setText('这个不可以哦，还没有准备好~QAQ~')
+            # 每次检测前先重置一下quit_flag，防止因为被修改为1而无法正常检测
+            quit_flag = 0
+            self.label_10.setVisible(0)
+            self.label_11.setVisible(0)
+            self.realtime_thread = RealtimeDetectThread()
+            self.realtime_thread.signal.connect(self.display)
+            self.realtime_thread.signal2.connect(self.print_ifo)
+            self.realtime_thread.start()
+
+
+class ImageDetectThread(QThread):
+    _signal = pyqtSignal(object)        # 发送信号,用于向主线程发送检测结果图片
+    _signal2 = pyqtSignal(object)       # 发送信号,用于向主线程发送检测结果数据
+
+    def __init__(self, parent=None):
+        super(ImageDetectThread, self).__init__(parent)
+
+    def run(self):
+        global model, device_id, quit_flag, conf_thres, iou_thres
+        # 初始化
+        result_label = []  # 空字符串存储检测结果
+        imgsz = 640
+        device = select_device(device_id)  # 设置设备
+        half = device.type != 'cpu'  # 有CUDA支持时使用半精度
+
+        # 实例化打开文件窗口
+        root = tk.Tk()
+        root.withdraw()
+
+        stride = int(model.stride.max())  # model stride
+        imgsz = check_img_size(imgsz, s=stride)  # 验证输入尺寸大小，如果不符合要求则进行自动调整
+        if half:
+            model.half()  # to FP16
+
+        try:
+            # 文件读取
+            # image_path = QFileDialog.getOpenFileName(self, '选择图片', '.', 'Image files (*.jpg)')[0]
+            image_path = filedialog.askopenfilename()
+            if image_path == '':
+                raise FileNotFoundError  # 如果未选择文件，即video_path为空，则主动抛出异常
+
+            t0 = time.time()  # 开始检测时间
+            # Set Dataloader
+            cudnn.benchmark = True  # set True to speed up constant image size inference
+            dataset = LoadImages(image_path, img_size=imgsz, stride=stride)
+
+            # Get names and colors
+            names = model.module.names if hasattr(model, 'module') else model.names
+            colors = [[random.randint(0, 255) for _ in range(3)] for _ in names]
+
+            # Run inference
+            if device.type != 'cpu':
+                model(torch.zeros(1, 3, imgsz, imgsz).to(device).type_as(next(model.parameters())))  # run once
+            for path, img, im0s, vid_cap, current_frame, total_frame in dataset:
+                img = torch.from_numpy(img).to(device)
+                img = img.half() if half else img.float()  # uint8 to fp16/32
+                img /= 255.0  # 0 - 255 to 0.0 - 1.0
+                if img.ndimension() == 3:
+                    img = img.unsqueeze(0)
+
+                # Inference
+                t1 = time_synchronized()
+                pred = model(img, augment=True)[0]  # augment默认为True,后续可根据要求更改
+
+                # Apply NMS
+                pred = non_max_suppression(pred, conf_thres/100, iou_thres/100, classes=None,
+                                           agnostic=False)
+                t2 = time_synchronized()
+
+                # Process detections
+                for i, det in enumerate(pred):  # detections per image
+                    p, s, im0, frame = path, '', im0s, getattr(dataset, 'frame', 0)
+
+                    s += '%gx%g ' % img.shape[2:]  # print string
+                    gn = torch.tensor(im0.shape)[[1, 0, 1, 0]]  # normalization gain whwh
+                    if len(det):
+                        # Rescale boxes from img_size to im0 size
+                        det[:, :4] = scale_coords(img.shape[2:], det[:, :4], im0.shape).round()
+
+                        # Print results
+                        for c in det[:, -1].unique():
+                            n = (det[:, -1] == c).sum()  # detections per class
+                            s += f"{n} {names[int(c)]}{'s' * (n > 1)}, "  # add to string
+
+                        # Write results
+                        for *xyxy, conf, cls in reversed(det):
+                            label = f'{names[int(cls)]} {conf:.2f}'
+                            result_label.append(label)
+                            plot_one_box(xyxy, im0, label=label, color=colors[int(cls)], line_thickness=3)
+
+                self._signal.emit(im0)
+
+                # 将检测结果的类别和置信度返回
+                s = ''  # 空字符串用于存储检测结果
+                for label in result_label:
+                    s = s + label + '\n'
+                self._signal2.emit(s)
+
+                t3 = time.time()  # 结束检测时间
+                print(f'{s}Inference+NMS: ({t2 - t1:.3f}s)')
+                print(f'总时长({t3 - t0:.3f}s)')
+
+                root.mainloop()
+
+        except FileNotFoundError:
+            print('请重新读取文件')
+
+    @property
+    def signal(self):
+        return self._signal
+
+    @property
+    def signal2(self):
+        return self._signal2
 
 
 class VideoDetectThread(QThread):
@@ -146,22 +246,15 @@ class VideoDetectThread(QThread):
     _signal2 = pyqtSignal(object)  # 发送信号,用于向主线程发送检测结果数据
     _signal3 = pyqtSignal(object)  # 发送信号,用于向主线程发送进度
 
-    def __init__(self, model, parent=None):
+    def __init__(self, parent=None):
         super(VideoDetectThread, self).__init__(parent)
-        self.model = model
 
     def run(self):
-        global quit_flag
-        global conf_thres
-        global iou_thres
-        global detect_frequency
-        global fps
+        global model, device_id, quit_flag, conf_thres, iou_thres, detect_frequency, fps
 
         # 初始化
-        model = self.model
-        device = '0'
         imgsz = 640
-        device = select_device(device)  # 设置设备
+        device = select_device(device_id)  # 设置设备
         half = device.type != 'cpu'  # 有CUDA支持时使用半精度
 
         # 实例化打开文件窗口
@@ -270,6 +363,115 @@ class VideoDetectThread(QThread):
     @property
     def signal3(self):
         return self._signal3
+
+
+class RealtimeDetectThread(QThread):
+    _signal = pyqtSignal(object)  # 发送信号,用于向主线程发送检测结果图片
+    _signal2 = pyqtSignal(object)  # 发送信号,用于向主线程发送检测结果数据
+
+    def __init__(self, parent=None):
+        super(RealtimeDetectThread, self).__init__(parent)
+
+    def run(self):
+        global model, device_id, quit_flag, conf_thres, iou_thres, detect_frequency, fps
+
+        # 初始化
+        imgsz = 640
+        device = select_device(device_id)  # 设置设备
+        half = device.type != 'cpu'  # 有CUDA支持时使用半精度
+
+        stride = int(model.stride.max())  # model stride
+        imgsz = check_img_size(imgsz, s=stride)  # 验证输入尺寸大小，如果不符合要求则进行自动调整
+        if half:
+            model.half()  # to FP16
+
+        try:
+            # 文件读取
+            camera = '0'
+
+            # Set Dataloader
+            cudnn.benchmark = True  # set True to speed up constant image size inference
+            dataset = LoadStreams(camera, img_size=imgsz, stride=stride)
+
+            # Get names and colors
+            names = model.module.names if hasattr(model, 'module') else model.names
+            colors = [[random.randint(0, 255) for _ in range(3)] for _ in names]
+
+            # Run inference
+            if device.type != 'cpu':
+                model(torch.zeros(1, 3, imgsz, imgsz).to(device).type_as(next(model.parameters())))  # run once
+            for path, img, im0s, vid_cap in dataset:
+                # 终止线程
+                if quit_flag == 1:
+                    quit_flag = 0
+                    print('已退出')
+                    break
+
+                t0 = time.time()  # 每帧开始检测时间
+                result_label = []
+                img = torch.from_numpy(img).to(device)
+                img = img.half() if half else img.float()  # uint8 to fp16/32
+                img /= 255.0  # 0 - 255 to 0.0 - 1.0
+                if img.ndimension() == 3:
+                    img = img.unsqueeze(0)
+
+                # Inference
+                t1 = time_synchronized()
+                pred = model(img, augment=True)[0]  # augment默认为True,后续可根据要求更改
+
+                # Apply NMS
+                pred = non_max_suppression(pred, conf_thres / 100, iou_thres / 100, classes=None,
+                                           agnostic=False)
+                t2 = time_synchronized()
+
+                # Process detections
+                for i, det in enumerate(pred):  # detections per image
+                    p, s, im0, frame = path[i], '%g: ' % i, im0s[i].copy(), dataset.count
+
+                    s += '%gx%g ' % img.shape[2:]  # print string
+                    gn = torch.tensor(im0.shape)[[1, 0, 1, 0]]  # normalization gain whwh
+                    if len(det):
+                        # Rescale boxes from img_size to im0 size
+                        det[:, :4] = scale_coords(img.shape[2:], det[:, :4], im0.shape).round()
+
+                        # Print results
+                        for c in det[:, -1].unique():
+                            n = (det[:, -1] == c).sum()  # detections per class
+                            s += f"{n} {names[int(c)]}{'s' * (n > 1)}, "  # add to string
+
+                        # Write results
+                        for *xyxy, conf, cls in reversed(det):
+                            label = f'{names[int(cls)]} {conf:.2f}'
+                            result_label.append(label)
+                            plot_one_box(xyxy, im0, label=label, color=colors[int(cls)], line_thickness=3)
+
+                self._signal.emit(im0)
+
+                # 将检测结果的类别和置信度返回
+                s = ''  # 空字符串用于存储检测结果
+                for label in result_label:
+                    s = s + label + '\n'
+                self._signal2.emit(s)
+
+                # 延时程序，达到指定时间后进入下一循环
+                while time.time() - t0 < 1 / fps:
+                    time.sleep(0.0001)
+
+                t3 = time.time()  # 结束检测时间
+                # Print time (inference + NMS)
+                print(f'{s}Inference+NMS:({t2 - t1:.3f}s)')
+                print(f'总用时({t3 - t0:.3f}s)')
+
+        except FileNotFoundError:
+            print('请重新读取文件')
+
+    @property
+    def signal(self):
+        return self._signal
+
+    @property
+    def signal2(self):
+        return self._signal2
 
 
 if __name__ == "__main__":
